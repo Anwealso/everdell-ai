43b38d26cbd5f558f5682dc8becb3704edca69f4
Anwealso <48880195+Anwealso@users.noreply.github.com>
2024-10-13 21:55:04 +1000

    Simplified api for class destination card

--- everdell-simulator/cards/DestinationCard.py
+++ everdell-simulator/cards/DestinationCard.py
@@ -1,6 +1,7 @@
 from Card import Card
 from Player import Player
 from Card import CardCategory
+from Worker import Worker
 from ActionSpaces.ActionSpace import CardActionSpace
 from resources import ResourceValue
 
@@ -12,7 +13,6 @@ class DestinationCard(Card):
 
     def __init__(
         self,
-        category: CardCategory,
         name: str,
         cost: ResourceValue,
         score_value: int,
@@ -20,7 +20,31 @@ class DestinationCard(Card):
         isOpen: bool,
         on_add_worker: function,
     ) -> None:
-        super(category, name, cost, score_value, on_play)
+        super(CardCategory.CONSTRUCTION, name, cost, score_value, on_play)
         self.isOpen: bool = isOpen
         self.action_space = CardActionSpace(on_add_worker, isOpen)
         pass
+
+    def hasAvailableSlot(self, player: Player) -> bool:
+        """
+        Checks whether the card's actionspace has an available slot for this specific player
+        """
+        return self.action_space.hasAvailableSlot(player)
+
+    def getWorkers(self) -> list[Worker]:
+        """
+        Checks whether the card's actionspace has an available slot for this specific player
+        """
+        return self.action_space.getWorkers()
+
+    def addWorker(self, worker: Worker) -> None:
+        """
+        Adds a worker to the card's actionspace
+        """
+        return self.action_space.addWorker(worker)
+
+    def removeAllWorkers(self) -> None:
+        """
+        Removes all the workers from the card's action space
+        """
+        return self.action_space.removeAllWorkers()
